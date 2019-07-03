d51a4f9a7de6a07deda510fbd56d53164968ace0
Liuce <yayv.cn@gmail.com>
2019-07-03 16:15:50 +0800

    最初的版本，可以发一个简单的请求了

---
+++ configs/cfg.default.php
@@ -0,0 +1,17 @@
+<?php
+
+$CONFIG = array();
+$CONFIG['theme'] = 'v/default';
+$CONFIG['compiled_template'] = 'v/_run';
+
+$CONFIG['baseurl'] = 'http://localhost/ApiTester/';
+$CONFIG['sitename'] = 'ApiTester';
+
+$CONFIG['database'] = array(
+	'host' 		=> '127.0.0.1',
+	'port' 		=> '3306',
+	'username' 	=> 'root',
+	'password' 	=> '1234',
+	'database' 	=> 'ApiTester',
+	);
+
